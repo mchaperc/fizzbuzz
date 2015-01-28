d48432d8b0f16e9978ac97bf574aa81dfb96bdde
mchaperc <mchaperc@yahoo.com>
2015-01-28 16:53:57 -0500

    Fixed append

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 	$('button').on('click', function() {
 		for (var i = 1; i <= 100; i++) {
 			if (i%5 === 0 && i%3 === 0) {
-				$('button').append('<p>Fizz Buzz</p>');
+				$('body').append('<p>Fizz Buzz</p>');
 			} else if (i%5 === 0) {
-				$('button').append('<p>Buzz</p>');
+				$('body').append('<p>Buzz</p>');
 			} else if (i%3 === 0) {
-				$('button').append('<p>Fizz</p>');
+				$('body').append('<p>Fizz</p>');
 			} else {
-				$('button').append('<p>' + i + '</p>');
+				$('body').append('<p>' + i + '</p>');
 			}
 		}
 	})
